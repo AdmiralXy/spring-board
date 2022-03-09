40365f58ea3057dc7f3c48ff6bf4d9b589b22c3f
AdmiralXy <gameproblem80@gmail.com>
2022-03-10 02:51:38 +0300

    add CSS security end-point

--- src/main/java/com/admiralxy/springboard/configuration/SecurityConfig.java
+++ src/main/java/com/admiralxy/springboard/configuration/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String HOME_ENDPOINT = "/";
     private static final String JS_FOLDER_ENDPOINT = "/js/**";
+    private static final String CSS_FOLDER_ENDPOINT = "/css/**";
     private static final String LOGIN_REGISTER_ENDPOINT = "/api/auth/**";
     private static final String ADMIN_ENDPOINT = "/api/admin/**";
     private static final String USER_ENDPOINT = "/api/**";
@@ -49,6 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers(HOME_ENDPOINT).permitAll()
                 .antMatchers(JS_FOLDER_ENDPOINT).permitAll()
+                .antMatchers(CSS_FOLDER_ENDPOINT).permitAll()
                 .antMatchers(LOGIN_REGISTER_ENDPOINT).permitAll()
                 .antMatchers(ADMIN_ENDPOINT).hasRole("ADMIN")
                 .antMatchers(USER_ENDPOINT).hasRole("USER")
